3960327586b1d02a9ac46feca93ed4fe0bf5351e
v.drachenko <only-victor@mail.ru>
2025-04-19 19:31:17 +0300

    Правки

--- crates/app-base/examples/app-base-example.c
+++ crates/app-base/examples/app-base-example.c
@@ -7,12 +7,14 @@
 #include <string.h>
 
 int main() {
-  dotenv(false);
-  log_init();
+    dotenv(false);
+    log_init();
 
-  log_msg(ERROR, "This is ERROR");
-  log_msg(WARN, "This is WARNING");
-  log_msg(INFO, "This is INFO");
-  log_msg(DEBUG, "This is DEBUG");
-  log_msg(TRACE, "This is TRACE");
+    log_msg(ERROR, "This is ERROR");
+    log_msg(WARN, "This is WARNING");
+    log_msg(INFO, "This is INFO");
+    log_msg(DEBUG, "This is DEBUG");
+    log_msg(TRACE, "This is TRACE");
+
+    malloc_stats();
 }
