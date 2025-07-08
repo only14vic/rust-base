dc05756336d9c3a8f64e79fea39ef9fcae480857
v.drachenko <only-victor@mail.ru>
2025-07-08 22:36:10 +0300

    Правка

--- crates/app/examples/app-example1.rs
+++ crates/app/examples/app-example1.rs
@@ -3,10 +3,8 @@ use {app::Config, app_base::prelude::*};
 fn main() -> Void {
     dotenv(false);
     let mut log = Logger::init()?;
-    let config = app::Config::load("app.ini")?;
+    let config = Config::load("app.ini")?;
     log.configure(&config.base.log)?;
-
-    dbg!(&config);
 
     ok()
 }
