1563f71229690ca0657a77a9178bc37a7c66ee6f
v.drachenko <only-victor@mail.ru>
2025-07-08 22:18:19 +0300

    Правка

--- crates/app/tests/tests_config.rs
+++ crates/app/tests/tests_config.rs
@@ -3,7 +3,7 @@ use {app::Config, app_base::prelude::*, std::env::set_current_dir};
 #[test]
 fn tests_config() -> Void {
     set_current_dir(env!("PWD"))?;
-    let _config = Config::load()?;
+    let _config = Config::load("app.ini")?;
 
     ok()
 }
