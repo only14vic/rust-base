46bd96e814d59a7441d02e0e92c1d4bc96feae22
v.drachenko <only-victor@mail.ru>
2025-07-10 08:51:43 +0300

    Правка

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,26 +1,34 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![no_main]
+#![cfg_attr(not(feature = "std"), no_main)]
 
 #[allow(unused_imports)]
 #[macro_use]
 extern crate alloc;
 extern crate core;
 
-use {
-    app_base::prelude::*,
-    core::ffi::{c_char, c_int}
-};
+#[cfg(not(feature = "std"))]
+use core::ffi::{c_char, c_int};
+
+use app_base::prelude::*;
 
 const CONFIG_FILE_NAME: &str = "app.ini";
 
+#[cfg(feature = "std")]
+fn main() -> Void {
+    run()
+}
+
+#[cfg(not(feature = "std"))]
 #[unsafe(no_mangle)]
 fn main(argc: usize, argv: *const *const c_char) -> c_int {
-    main_run(argc, argv).inspect_err(|e| panic!("{e}")).ok();
+    run(argc, argv).inspect_err(|e| panic!("{e}")).ok();
     libc::EXIT_SUCCESS
 }
 
-#[allow(unused_variables)]
-fn main_run(argc: usize, argv: *const *const c_char) -> Void {
+fn run(
+    #[cfg(not(feature = "std"))] argc: usize,
+    #[cfg(not(feature = "std"))] argv: *const *const c_char
+) -> Void {
     dotenv(false);
     let mut log = Logger::init()?;
 
