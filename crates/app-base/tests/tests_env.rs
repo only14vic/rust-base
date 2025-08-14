c07e4101d2e1f5fdb2ee2640dd7f164edb4ae022
v.drachenko <only-victor@mail.ru>
2025-08-14 10:52:16 +0300

    Правка

--- crates/app-base/tests/tests_env.rs
+++ crates/app-base/tests/tests_env.rs
@@ -1,21 +1,40 @@
-use {app_base::prelude::Env, core::hint::black_box, std::time::Instant};
+use {
+    app_base::prelude::*,
+    core::hint::black_box,
+    std::{sync::Barrier, time::Instant}
+};
+
+const THREADS_COUNT: usize = 4;
+const MAX_ITERS: usize = 1_000_000;
 
 #[test]
 fn test_env() {
     let env = Env::default();
     dbg!(&env);
 
-    let t = Instant::now();
-    for _ in 0..1_000_000 {
-        black_box({
-            assert_eq!(env.env, Env::env());
-            assert_eq!(env.is_test, Env::is_test());
-            assert_eq!(env.is_dev, Env::is_dev());
-            assert_eq!(env.is_prod, Env::is_prod());
-            assert_eq!(env.is_debug, Env::is_debug());
-            assert_eq!(env.is_release, Env::is_release());
-        });
-    }
+    let barrier = Barrier::new(THREADS_COUNT);
+    let ta = Instant::now();
 
-    dbg!(t.elapsed());
+    std::thread::scope(|s| {
+        for _ in 0..THREADS_COUNT {
+            s.spawn(|| {
+                barrier.wait();
+                let t = Instant::now();
+                for _ in 0..MAX_ITERS {
+                    black_box({
+                        assert_eq!(env.env, Env::env());
+                        assert_eq!(env.is_test, Env::is_test());
+                        assert_eq!(env.is_dev, Env::is_dev());
+                        assert_eq!(env.is_prod, Env::is_prod());
+                        assert_eq!(env.is_debug, Env::is_debug());
+                        assert_eq!(env.is_release, Env::is_release());
+                    });
+                }
+
+                dbg!(t.elapsed());
+            });
+        }
+    });
+
+    dbg!(ta.elapsed());
 }
