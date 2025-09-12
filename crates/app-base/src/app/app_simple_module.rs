65fda99dc62d9280986cd8fa5c5478ad98441456
v.drachenko <only-victor@mail.ru>
2025-09-12 18:11:40 +0300

    Правка

--- crates/app-base/src/app/app_simple_module.rs
+++ crates/app-base/src/app/app_simple_module.rs
@@ -27,17 +27,17 @@ Usage: {bin} [command] [options]
 Version: {name} ({version})
 
 Commands:
-    {cmd:<len$} - {desc} (default)
+    {:<len$} - {} (default)
 
 Options:
     -h, --help  - show usage help
 "#,
+            Self::COMMAND,
+            Self::DESCRIPTION,
             len = 5,
             bin = config.dirs.exe_file(),
             version = config.base.version,
             name = config.base.name,
-            cmd = Self::COMMAND,
-            desc = Self::DESCRIPTION
         );
 
         ok()
